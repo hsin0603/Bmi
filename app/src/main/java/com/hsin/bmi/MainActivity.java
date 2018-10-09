3214fe2eb8041a0493f93d65f5a1f298ae5fc97c
hsin0603 <gemini880603@gmail.com>
2018-10-09 16:21:11 +0800

    Toast,Alert

--- app/src/main/java/com/hsin/bmi/MainActivity.java
+++ app/src/main/java/com/hsin/bmi/MainActivity.java
@@ -1,28 +1,48 @@
 package com.hsin.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
     }
     public void bmi(View view){
         Log.d("MainActivity","bmi");
-        EditText edWeight = findViewById(R.id.ed_weight);
-        EditText edheight = findViewById(R.id.ed_height);
         String w = edWeight.getText().toString();
-        String h = edheight.getText().toString();
+        String h = edHeight.getText().toString();
         Log.d("MainActivity",w + "/" + h);
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight / (height * height);
         Log.d("MainActivity",bmi+"");
+        Toast.makeText(this,"Your BMI is " + bmi,
+                Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is" + bmi)
+                .setTitle("BMI")
+                .setPositiveButton("OK",null)
+                .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edWeight.setText("");
+                        edHeight.setText("");
+                    }
+                })
+                .show();
     }
 }
